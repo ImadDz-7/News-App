bb935e0f583b4dcb508e42d6e45a7b9644b1b4eb
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-06-30 19:26:53 +0000

    Add assets folder

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class NewsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(),
     );
   }
