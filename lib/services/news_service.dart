0fd9d8a641273a649bca11ac61d80a083f784148
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-07-02 07:24:24 +0000

    Create getTopHeadlines methode and make the request and get the response successfully and print it in debugConsole

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -1,3 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:news_app/models/article_model.dart';
+
 class NewsService{
-  
+  final Dio dio;
+
+  NewsService(this.dio);
+
+  getTopHeadlines() async{
+    try{
+      Response response = await dio.get('https://newsapi.org/v2/top-headlines?apiKey=8b4c4c0281e440098baccbe18bd1c4f9&category=general&country=us');
+      Map<String, dynamic> jsonData = response.data;
+      List<dynamic> articles = jsonData['articles']; 
+      print(articles);
+    }
+    catch(e){}
+  }
 }
