3a1c36d79b46a31bf202500336c5fdd3382c1819
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-07-02 07:31:29 +0000

    Finish getTopHeadlines methode

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -1,18 +1,26 @@
 import 'package:dio/dio.dart';
 import 'package:news_app/models/article_model.dart';
 
-class NewsService{
+class NewsService {
   final Dio dio;
 
   NewsService(this.dio);
 
-  getTopHeadlines() async{
-    try{
-      Response response = await dio.get('https://newsapi.org/v2/top-headlines?apiKey=8b4c4c0281e440098baccbe18bd1c4f9&category=general&country=us');
+  Future<List<ArticleModel>> getTopHeadlines() async {
+    try {
+      Response response = await dio.get(
+          'https://newsapi.org/v2/top-headlines?apiKey=8b4c4c0281e440098baccbe18bd1c4f9&category=general&country=us');
       Map<String, dynamic> jsonData = response.data;
-      List<dynamic> articles = jsonData['articles']; 
-      print(articles);
+      List<dynamic> articles = jsonData['articles'];
+
+      List<ArticleModel> articlesList = [];
+      for (var article in articles) {
+        ArticleModel articleModel = ArticleModel.fromJson(article);
+        articlesList.add(articleModel);
+      }
+      return articlesList;
+    } catch (e) {
+      return [];
     }
-    catch(e){}
   }
 }
