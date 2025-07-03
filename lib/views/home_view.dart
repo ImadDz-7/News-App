79cca7ec12b6d9e97365f88d3160d831900c9064
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-07-03 06:08:23 +0000

    Create CategoryView

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/widgets/categories_list_view.dart';
-import 'package:news_app/widgets/category_card.dart';
-import 'package:news_app/widgets/news_list_view.dart';
 import 'package:news_app/widgets/news_list_view_builder.dart';
-import 'package:news_app/widgets/news_tile.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -28,10 +25,10 @@ class HomeView extends StatelessWidget {
           ],
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
         child: CustomScrollView(
-          physics: const BouncingScrollPhysics(),
+          physics: BouncingScrollPhysics(),
           slivers: [
             SliverToBoxAdapter(
               child: CategoriesListView(),
--- lib/widgets/news_tile.dart
+++ lib/widgets/news_tile.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/models/article_model.dart';
 
@@ -10,14 +11,26 @@ class NewsTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        // ClipRRect(
+        //   borderRadius: BorderRadius.circular(8),
+        //   child: CachedNetworkImage(
+        //     height: 196,
+        //     width: double.infinity,
+        //     fit: BoxFit.fill,
+        //     imageUrl: article.image,
+        //     placeholder: (context, url) => const CircularProgressIndicator(),
+        //     errorWidget: (context, url, error) => Icon(Icons.error),
+        //   ),
+        // ),
         ClipRRect(
           borderRadius: BorderRadius.circular(8),
           child: Image.network(
-              height: 195,
-              width: double.infinity,
-              fit: BoxFit.fill,
-              article.image ??
-                  'https://media.istockphoto.com/id/1353858440/vector/abstract-breaking-news-modern-background-wallpaper-new-media-backdrop-for-news-and-channel.jpg?s=612x612&w=0&k=20&c=Qz30hGfczQm1mgtnp-o3Pq-tb6ycEYEjPgMHlnqY_HM='),
+            height: 195,
+            width: double.infinity,
+            fit: BoxFit.fill,
+            article.image ??
+                'https://media.istockphoto.com/id/1353858440/vector/abstract-breaking-news-modern-background-wallpaper-new-media-backdrop-for-news-and-channel.jpg?s=612x612&w=0&k=20&c=Qz30hGfczQm1mgtnp-o3Pq-tb6ycEYEjPgMHlnqY_HM=',
+          ),
         ),
         const SizedBox(height: 5),
         Text(
